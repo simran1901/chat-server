ccbd939393c33ade44ecdf4060ca4b16434318d7
Simran Makhijani <simranmakhijani55@gmail.com>
2021-08-16 22:59:37 +0530

    one to one chat

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ var server = http.createServer(app);
 var io = require('socket.io')(server);
 
 app.use(express.json());
+var clients = {};
 
 io.on('connection', (socket) => {
     console.log('connected');
     console.log(socket.id, ' has joined');
-    socket.on('/test', (msg)=>{
+    socket.on('signin', (id) => {
+        console.log(id);
+        clients[id] = socket;
+        console.log(clients);
+    });
+    socket.on('message', (msg) => {
         console.log(msg);
+        let targetId = msg.targetId;
+        if (clients[targetId])
+            clients[targetId].emit('message', msg);
     });
 });
 
