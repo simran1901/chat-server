32a2ca9a61239b4528683c161838342a07d7adc6
Simran Makhijani <simranmakhijani55@gmail.com>
2021-09-01 23:55:28 +0530

    image sharing

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var io = require('socket.io')(server);
 
 app.use(express.json());
 var clients = {};
+const routes = require("./routes");
+app.use("/routes", routes);
 
 io.on('connection', (socket) => {
     console.log('connected');
